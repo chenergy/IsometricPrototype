4b955e284df545f8f56fd389cb9ece5e589770da
Jonathan Chien <jonathan.chien@gmail.com>
2015-05-12 18:25:35 -0700

    fixed placement bug (cannot place another object while already placing)

--- Assets/Scripts/ConstructionController.cs
+++ Assets/Scripts/ConstructionController.cs
@@ -49,55 +49,45 @@ public class ConstructionController : MonoBehaviour
 
 	void TouchDrag_OnPointerDownEvent (PointerEventData p, A_TouchDrag td) {
 		if (this.state == ConstructionState.SELECTING) {
+			// Disable all further construction, except for current button.
+			foreach (A_TouchDrag button in this.constructionButtons){
+				if (button != td)
+					button.eventTrigger.enabled = false;
+			}
+			
+			// Create the building passed by the touch drag button.
 			TouchDrag_Button tdb = (TouchDrag_Button)td;
-	
 			if (tdb != null) {
-				// Disable all further construction, except for current button.
-				foreach (A_TouchDrag button in this.constructionButtons){
-					if (button != td)
-						button.eventTrigger.enabled = false;
-				}
+				this.CreateBuilding (tdb.prefabToCreate);
+			}
+		}
+	}
 	
-				// Create a new building from touchdrag data.
-				Building b = this.CreateBuilding (tdb.prefabToCreate);
-				if (b != null) {
-					// Set state to placing.
-					this.state = ConstructionState.PLACING;
-					
-					// Select new building and set base tile to (0, 0).
-					this.SelectBuilding (b);
-					b.SetBaseTile (this.tc.GetTile (new IntVector2 (0, 0)));
-					
-					// Show which tiles are being used.
-					this.ShowAllUsedTiles();
-					
-					// Open menu for the building.
-					this.buildingMenu.Show (b, this.state);
+	
+	void TouchDrag_OnDragEvent (PointerEventData p, A_TouchDrag td){
+		if (this.state == ConstructionState.PLACING) {
+			Tile targetTile = this.RaycastHitTile (p);
+
+			// Building is being selected, and it has a target tile.
+			if (targetTile != null && this.selectedBuilding != null) {
+				// Only move if base tiles of building has changed.
+				if (targetTile != this.selectedBuilding.BaseTile) {
+					// Drag the selected building to the tile.
+					this.MoveBuilding (this.selectedBuilding, targetTile);
 				}
 			}
 		}
 	}
-
-
+	
+	
 	void TouchDrag_OnPointerUpEvent (PointerEventData p, A_TouchDrag td) {
 		if (this.state == ConstructionState.PLACING) {
 			// Disable touch drag on pointer up.
 			TouchDrag_Button tdb = (TouchDrag_Button)td;
 			tdb.eventTrigger.enabled = false;
 			
-			// Enable the building's collider so that it can be selected again.
-			if (this.selectedBuilding != null) {
-				Collider c = this.selectedBuilding.GetComponent <Collider> ();
-				if (c != null)
-					c.enabled = true;
-			}
-		}
-	}
-
-
-	void TouchDrag_OnDragEvent (PointerEventData p, A_TouchDrag td){
-		if (this.state == ConstructionState.PLACING) {
-			this.DragBuilding (p);
+			// Drop the building.
+			this.DropBuilding (this.selectedBuilding);
 		}
 	}
 	
@@ -105,37 +95,8 @@ public class ConstructionController : MonoBehaviour
 	public void EventTrigger_OnPointerDownEvent (PointerEventData p) {
 		// In placement mode, the selected building needs to be dragged. 
 		if (this.state == ConstructionState.PLACING) {
-			// Disable the building's collider in order to get tile collision.
 			if (this.selectedBuilding != null) {
-				Collider c = this.selectedBuilding.GetComponent <Collider> ();
-				if (c != null)
-					c.enabled = false;
-			}
-		}
-	}
-	
-	
-	public void EventTrigger_OnPointerUpEvent (PointerEventData p, bool hasBeenDragged){
-		if (hasBeenDragged) {
-			// In placement mode, the selected building needs a collider to be selected again. 
-			if (this.state == ConstructionState.PLACING) {
-				// Enable the building's collider so that raycast can find it.
-				if (this.selectedBuilding != null) {
-					Collider c = this.selectedBuilding.GetComponent <Collider> ();
-					if (c != null)
-						c.enabled = true;
-				}
-			}
-		} else {
-			// In selection mode, select a target building or deselect it.
-			if (this.state == ConstructionState.SELECTING) {
-				Building b = this.RaycastHitBuilding (p);
-				if (b != null) 
-					//this.OnBuildingSelectEvent (b);
-					this.SelectBuilding (b);
-				else 
-					//this.OnBuildingDeselectEvent ();
-					this.DeselectBuilding ();
+				this.DisableBuildingCollider (this.selectedBuilding);
 			}
 		}
 	}
@@ -143,99 +104,84 @@ public class ConstructionController : MonoBehaviour
 	
 	public void EventTrigger_OnDragEvent (PointerEventData p){
 		// In placement mode, drag the building being placed.
-		if (this.state == ConstructionState.PLACING)
-			this.DragBuilding (p);
-	}
+		if (this.state == ConstructionState.PLACING) {
+			Tile targetTile = this.RaycastHitTile (p);
 
-
-	public void Placing_OnClickPlaceEvent () {
-		if (this.CanBuildingBePlaced (this.selectedBuilding)) {
-			// Hide all used tiles.
-			this.HideAllUsedTiles ();
-
-			// Re-enable touchdrag buttons.
-			foreach (TouchDrag_Button tdb in this.constructionButtons) {
-				tdb.eventTrigger.enabled = true;
-			}
-
-			// Add the placed building to the list if necessary.
-			if (!this.placedBuildings.Contains (this.selectedBuilding)) {
-				this.placedBuildings.Add (this.selectedBuilding);
-				foreach (IntVector2 v in this.selectedBuilding.UsedTileLocations){
-					this.tc.SetTileInUse (v, true);
+			// Building is being selected, and it has a target tile.
+			if (targetTile != null && this.selectedBuilding != null) {
+				// Only move if base tiles of building has changed.
+				if (targetTile != this.selectedBuilding.BaseTile) {
+					// Drag the selected building to the tile.
+					this.MoveBuilding (this.selectedBuilding, targetTile);
 				}
 			}
-
-			// Save before reseting.
-			//Building temp = this.selectedBuilding;
-
-			// End click event.
-			//this.EndClickEvent ();
-			
-			// Set state to selecting.
-			this.state = ConstructionState.SELECTING;
-
-			// Reset object.
-			//temp.PlaceDown ();
-			this.selectedBuilding.PlaceDown ();
-			
-			// Turn off building menu.
-			this.buildingMenu.Show (this.selectedBuilding, this.state);
+		}
+	}
+	
+	
+	public void EventTrigger_OnPointerUpEvent (PointerEventData p, bool hasBeenDragged){
+		// In placement mode.
+		if (this.state == ConstructionState.PLACING) {
+			if (hasBeenDragged) {
+				// The selected building needs a collider to be selected again. 
+				this.EnableBuildingCollider (this.selectedBuilding);
+			}
+		} 
+		// In selecting mode.
+		else if (this.state == ConstructionState.SELECTING) {
+			if (!hasBeenDragged) {
+				// Select a target building or deselect it.
+				Building b = this.RaycastHitBuilding (p);
+				if (b != null) 
+					this.SelectBuilding (b);
+				else 
+					this.DeselectBuilding ();
+			}
+		}
+		
+		/*
+		if (hasBeenDragged) {
+			// In placement mode, the selected building needs a collider to be selected again. 
+			if (this.state == ConstructionState.PLACING) {
+				this.EnableBuildingCollider (this.selectedBuilding);
+			}
 		} else {
-			Debug.Log ("cannot place building in location");
+			// In selection mode, select a target building or deselect it.
+			if (this.state == ConstructionState.SELECTING) {
+				Building b = this.RaycastHitBuilding (p);
+				if (b != null) 
+					this.SelectBuilding (b);
+				else 
+					this.DeselectBuilding ();
+			}
+		}
+		*/
+	}
+	
+	
+	public void Placing_OnClickPlaceEvent () {
+		if (this.state == ConstructionState.PLACING) {
+			this.PlaceBuilding (this.selectedBuilding);
 		}
 	}
 	
 	
 	public void Placing_OnClickCancelEvent (){
-		// Re-enable touchdrag buttons.
-		foreach (TouchDrag_Button tdb in this.constructionButtons) {
-			tdb.eventTrigger.enabled = true;
-		}
-		
-		// Reset the selector parent.
-		this.ReleaseSelector ();
-		
-		// Save before reseting.
-		//Building temp = this.selectedBuilding;
-
-		// End click event.
-		//this.EndClickEvent ();
-		
-		// Set state to selecting.
-		this.state = ConstructionState.SELECTING;
-
-		// Hide all used tiles.
-		this.HideAllUsedTiles ();
-		
-		// Reset object.
-		//temp.Reset ();
-		this.selectedBuilding.Reset ();
-		
-		//this.selectedBuilding = null;
-		
-		//Debug.Log (this.selectedBuilding.buildingName);
+		if (this.state == ConstructionState.PLACING){
+			// Re-enable touchdrag buttons.
+			foreach (TouchDrag_Button tdb in this.constructionButtons) {
+				tdb.eventTrigger.enabled = true;
+			}
 			
-		// Turn off building menu. Close menu if object has been deleted.
-		if (this.selectedBuilding != null) {
-			this.buildingMenu.Show (this.selectedBuilding, this.state);
-			this.BindSelector (this.selectedBuilding);
-		} else {
-			this.buildingMenu.Close ();
+			this.ResetBuilding ();
 		}
 	}
 
 	
 	public void Selecting_OnClickMoveEvent () {
-		this.state = ConstructionState.PLACING;
-		
-		foreach (IntVector2 v in this.selectedBuilding.UsedTileLocations){
-			this.tc.SetTileInUse (v, false);
+		if (this.state == ConstructionState.SELECTING) {
+			this.LiftBuilding (this.selectedBuilding);
 		}
-		
-		this.ShowAllUsedTiles ();
-		
-		this.buildingMenu.Show (this.selectedBuilding, this.state);
 	}
 	
 	
@@ -250,23 +196,57 @@ public class ConstructionController : MonoBehaviour
 		this.buildingMenu.Close ();
 	}
 	
-
-
-	/*void EndClickEvent (){
+	
+	void CreateBuilding (GameObject prefab) {
+		// Instantiate object.
+		GameObject objectToCreate = GameObject.Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
 		
+		// Create a new building from touchdrag data.
+		Building b = objectToCreate.GetComponent <Building> ();
+		if (b != null) {
+			// Set state to placing.
+			this.state = ConstructionState.PLACING;
+			
+			// Select new building and set base tile to (0, 0).
+			this.SelectBuilding (b);
+			b.SetBaseTile (this.tc.GetTile (new IntVector2 (0, 0)));
+			
+			// Show which tiles are being used.
+			this.ShowAllUsedTiles();
+			
+			// Open menu for the building.
+			this.buildingMenu.Show (b, this.state);
+		}
+	}
+	
+	
+	void LiftBuilding (Building b){
+		this.state = ConstructionState.PLACING;
 		
+		// Disable all further construction.
+		foreach (A_TouchDrag button in this.constructionButtons){
+			button.eventTrigger.enabled = false;
+		}
 		
-		//this.buildingMenu.Close ();
-	}*/
-
-
+		// Set the given building's used tiles as not used.
+		foreach (IntVector2 v in b.UsedTileLocations){
+			this.tc.SetTileInUse (v, false);
+		}
+		
+		// Show used tiles.
+		this.ShowAllUsedTiles ();
+		
+		// Show the placing building menu.
+		this.buildingMenu.Show (b, this.state);
+	}
+	
+	
+	
 	void SelectBuilding (Building b) {
 		this.selectedBuilding = b;
 		this.buildingMenu.Show (b, this.state);
-		//this.ShowAllUsedTiles ();
 		this.BindSelector (b);
 	}
-	
 	
 	
 	void DeselectBuilding () {
@@ -275,37 +255,109 @@ public class ConstructionController : MonoBehaviour
 		this.HideAllUsedTiles ();
 		this.ReleaseSelector ();
 	}
+	
+	
+	void MoveBuilding (Building b, Tile t){
+		// Hide current building tiles.
+		this.HideBuildingUsedTiles (b);
 
+		// Move building to the tile location.
+		b.transform.position = t.transform.position;
 
-	Building CreateBuilding (GameObject prefab) {
-		// Instantiate object.
-		GameObject objectToCreate = GameObject.Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
-		return objectToCreate.GetComponent <Building> ();
+		// Update base tile.
+		b.SetBaseTile (t);
+	
+		// Show selected tiles.
+		this.ShowAllUsedTiles ();
 	}
 	
-	void DragBuilding (PointerEventData p){
-		Tile targetTile = this.RaycastHitTile (p);
-
-		// Building is being selected, and it has a target tile.
-		if (targetTile != null && this.selectedBuilding != null) {
-			// Only move if base tiles of building has changed.
-			if (targetTile != this.selectedBuilding.BaseTile) {
-				// Hide current building tiles.
-				this.HideBuildingUsedTiles (this.selectedBuilding);
-
-				// Move building to the tile location.
-				this.selectedBuilding.transform.position = targetTile.transform.position;
-
-				// Update base tile.
-				this.selectedBuilding.SetBaseTile (targetTile);
-			
-				// Show selected tiles.
-				this.ShowAllUsedTiles ();
-			}
+	
+	void DropBuilding (Building b) {
+		// Enable the building's collider so that it can be selected again.
+		if (b != null) {
+			Collider c = b.GetComponent <Collider> ();
+			if (c != null)
+				c.enabled = true;
 		}
 	}
+	
+	
+	void PlaceBuilding (Building b){
+		if (this.CanBuildingBePlaced (b)) {
+			// Hide all used tiles.
+			this.HideAllUsedTiles ();
 
+			// Re-enable touchdrag buttons.
+			foreach (TouchDrag_Button tdb in this.constructionButtons) {
+				tdb.eventTrigger.enabled = true;
+			}
 
+			// Add the placed building to the list if necessary.
+			if (!this.placedBuildings.Contains (b)) {
+				this.placedBuildings.Add (b);
+			}
+			
+			// Set tiles as in-use.
+			foreach (IntVector2 v in b.UsedTileLocations){
+				this.tc.SetTileInUse (v, true);
+			}
+			
+			// Set state to selecting.
+			this.state = ConstructionState.SELECTING;
+
+			// Reset object.
+			//temp.PlaceDown ();
+			this.selectedBuilding.PlaceDown ();
+			
+			// Turn off building menu.
+			this.buildingMenu.Show (b, this.state);
+		} else {
+			Debug.Log ("cannot place building in location");
+		}
+	}
+	
+	
+	void ResetBuilding (){
+		// Reset the selector parent.
+		this.ReleaseSelector ();
+		
+		// Set state to selecting.
+		this.state = ConstructionState.SELECTING;
+
+		// Hide all used tiles.
+		this.HideAllUsedTiles ();
+		
+		// Reset object.
+		this.selectedBuilding.Reset ();
+			
+		// Turn off building menu. Close menu if object has been deleted.
+		if (this.selectedBuilding != null) {
+			this.buildingMenu.Show (this.selectedBuilding, this.state);
+			this.BindSelector (this.selectedBuilding);
+		} else {
+			this.buildingMenu.Close ();
+		}
+	}
+	
+	
+	void EnableBuildingCollider (Building b) {
+		// Enable the building's collider so that raycast can find it.
+		if (b != null) {
+			Collider c = b.GetComponent <Collider> ();
+			if (c != null)
+				c.enabled = true;
+		}
+	}
+	
+	
+	void DisableBuildingCollider (Building b) {
+		// Disable the building's collider in order to get tile collision beneath.
+		Collider c = b.GetComponent <Collider> ();
+		if (c != null)
+			c.enabled = false;
+	}
+	
+	
 	bool CanBuildingBePlaced (Building b){
 		foreach (IntVector2 v in b.UsedTileLocations) {
 			Tile t = this.tc.GetTile (v);
